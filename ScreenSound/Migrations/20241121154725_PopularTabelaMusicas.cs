2696b16714b4d863424bc7bfba718c2ee88d960d
cianciya <yagogarsoncianci@gmail.com>
2024-11-21 13:50:57 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Migrations/20241121154725_PopularTabelaMusicas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Oceano", "1989" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE * FROM Musicas");
+        }
+    }
+}
